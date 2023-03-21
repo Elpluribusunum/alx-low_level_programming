0c760c2336129ebc98f11e522227b3957296e256
Elpluribusunum <iyogunclarkson@gmail.com>
2023-03-21 16:17:38 -0700

    God Is Good

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,10 +3,9 @@
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
+ * Return: 1 for positive num, -1 for negative num or zero for anything else
  */
+
 int print_sign(int n)
 
 {
@@ -16,7 +15,7 @@ int print_sign(int n)
 	_putchar(43);
 	return (1);
 	}
-       	else if (n < 0)
+	else if (n < 0)
 	{
 	_putchar(45);
 	return (-1);
