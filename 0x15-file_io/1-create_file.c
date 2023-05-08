7308b4964ffddd6b908eac838ea5e0d0f94fe3b0
Elpluribusunum <iyogunclarkson@gmail.com>
2023-05-08 15:51:18 -0700

    Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - makes a new file.
+ * @filename: a pointer to the file's name that has to be created.
+ * @text_content: a file-writing pointer to a string
+ *
+ * Return: 1 if the function succeeds,
+ *         -1 otherwise.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
